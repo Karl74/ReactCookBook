91ec92d37d3f5abb10d37b0e7bd163d60c41fe04
Karl74 <carlos.castro74@gmail.com>
2019-04-19 16:53:44 -0500

    final component

--- view/components/Instructions.js
+++ view/components/Instructions.js
@@ -1,7 +1,13 @@
 
 
-const Instructions = ()=>
-
-	<p>Hello I am Instructions </p>
+const Instructions = ({steps})=>
+	<div>
+		<h1>Cooking Instructions</h1>
+		<div>
+			{steps.map((step, i)=>
+				<p key={i}>{step}</p>)
+			}
+		</div>
+	</div>
 
 export default Instructions
